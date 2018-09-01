0a2470ae2b20f46cbcfcabecc8a36b1d9e887b0a
moses <mwasmoxe@gmail.com>
2018-09-01 17:52:40 +0300

    logic code

---
+++ logic.py
@@ -0,0 +1,8 @@
+name = input('Enter your name: ')
+
+if name.upper() == 'MOSE':
+    password = input('Enter the password: ')
+    if password == 'love':
+        print('its you')
+else:
+    print('You are not mose.')
