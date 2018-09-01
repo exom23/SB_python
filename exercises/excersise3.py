845f654abf325c0ed75b53ab22ce624bf0f3670f
moses <mwasmoxe@gmail.com>
2018-09-01 16:35:52 +0300

    exercises from automated boring stuff

---
+++ exercises/excersise3.py
@@ -0,0 +1,11 @@
+print(' * ', sep='')
+print(' ** ')
+print(' *** ')
+print(' ***** ')
+print(' ****** ')
+
+3. Print a triangle like the one below.
+*
+**
+***
+****
---
+++ exercises/excrise 5.py
@@ -0,0 +1,2 @@
+print('Enter a number: 5' ,sep = '')
+print('The square of 5 is 25.')
