c6be353b488fd689ca21ead75514d9ade7581e56
moses <mwasmoxe@gmail.com>
2018-09-01 18:49:25 +0300

    snap game code

---
+++ snap.py
@@ -0,0 +1,13 @@
+import time
+import random
+print('Welcome to Self Timer')
+print()
+print('Everybody stand up')
+print('Stay standing until you think the time has ended')
+print('Then sit down.')
+print('Anyone still standing when the time ends loses.')
+print('The last person to sit down before the time ended will win.')
+stand_time = random.randint(5, 20)
+print('Stay standing for',stand_time, 'seconds.')
+time.sleep(stand_time)
+print('***TIMES UP***')
